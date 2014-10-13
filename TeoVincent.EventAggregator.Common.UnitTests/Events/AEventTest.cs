42d84c8c4ac67f95c825845b8185d83b66d4d5c5
TeoVincent <artur.wincenciak@gmail.com>
2014-10-13 21:12:06 +0200

    Change MSTest to xUnit

--- TeoVincent.EventAggregator.Common.UnitTests/Events/AEventTest.cs
+++ TeoVincent.EventAggregator.Common.UnitTests/Events/AEventTest.cs
@@ -24,13 +24,12 @@
 // SOFTWARE.
 #endregion
 using TeoVincent.EventAggregator.Common.Events;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using TeoVincent.EventAggregator.Common.UnitTests.Events.Mock;
+using Xunit;
 
 namespace TeoVincent.EventAggregator.Common.UnitTests.Events
 {
-    [TestClass]
 	public class AEventTest
 	{
 		internal AEvent CreateEvent()
@@ -41,110 +40,114 @@ namespace TeoVincent.EventAggregator.Common.UnitTests.Events
 
 		#region Equals(AEvent) Method Test
 
-		[TestMethod()]
-		public void EqualEventWith_The_Same_Type_Test()
+		[Fact]
+		public void Equal_Event_With_The_Same_Type_Test()
 		{
 			// 1) arrange
             AEvent target = CreateEvent();
 			AEvent other = new SimpleEventMock();
 
-			bool expected = false;
-			bool actual;
+            // 2) act
+			bool actual = target.Equals(other);
 
-            // act
-			actual = target.Equals(other);
-
-            // assert
-			Assert.AreEqual(expected, actual);
+            // 3) assert
+            Assert.False(actual);
 		}
 
-		[TestMethod()]
-		public void EqualEvent_With_The_Same_IdTest()
+        [Fact]
+		public void Equal_Event_With_The_Same_Id_Test()
 		{
-			AEvent target = CreateEvent();
+            // 1) arrange
+            AEvent target = CreateEvent();
 			AEvent other = new SimpleEventMock();
 			other.ID = target.ID;
 
-			bool expected = true;
-			bool actual;
+            // 2) act
+			bool actual = target.Equals(other);
 
-			actual = target.Equals(other);
-			Assert.AreEqual(expected, actual);
+            // 3) assert
+            Assert.True(actual);
 		}
 
-		[TestMethod()]
-		public void EqualEvent_After_Copy_ReferenceTest()
+        [Fact]
+		public void Equal_Event_After_Copy_Reference_Test()
 		{
-			AEvent target = CreateEvent();
+            // 1) arrange
+            AEvent target = CreateEvent();
 			AEvent other = target;
 
-			bool expected = true;
-			bool actual;
+            // 2) act
+			bool actual = target.Equals(other);
 
-			actual = target.Equals(other);
-			Assert.AreEqual(expected, actual);
+            // 3) assert
+            Assert.True(actual);
 		}
 
-		[TestMethod()]
-		public void EqualEvent_With_The_Same_IdBut_With_Different_TypeTest()
+        [Fact]
+		public void Equal_Event_With_The_Same_Id_But_With_Different_Type_Test()
 		{
-			AEvent target = CreateEvent();
+            // 1) arrange
+            AEvent target = CreateEvent();
 			target.ID = new Guid(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 			AEvent other = new OtherSimpleEventMock();
 			other.ID = new Guid(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 
-			bool expected = false;
-			bool actual;
+            // 2) act
+			bool actual = target.Equals(other);
 
-			actual = target.Equals(other);
-			Assert.AreEqual(expected, actual);
+            // 3) assert
+            Assert.False(actual);
 		}
 
 		#endregion
 
 		#region Equals(object) Method Test
 
-		[TestMethod()]
-		public void EqualObject_Test()
+        [Fact]
+		public void Equal_Object_Test()
 		{
-			AEvent target = CreateEvent();
+            // 1) arrange
+            AEvent target = CreateEvent();
 			object obj = new SimpleEventMock();
 
-			bool expected = false;
-			bool actual;
+            // 2) act
+			bool actual = target.Equals(obj);
 
-			actual = target.Equals(obj);
-			Assert.AreEqual(expected, actual);
+            // 3) assert
+            Assert.False(actual);
 		}
 
-		[TestMethod()]
-		public void EqualObject_With_The_Same_IdTest()
+        [Fact]
+		public void Equal_Object_With_The_Same_Id_Test()
 		{
-			AEvent target = CreateEvent();
+            // 1) arrange
+            AEvent target = CreateEvent();
 			object other = new SimpleEventMock();
 			((AEvent)other).ID = target.ID;
 
-			bool expected = true;
-			bool actual;
+            // 2) act
+			bool actual = target.Equals(other);
 
-			actual = target.Equals(other);
-			Assert.AreEqual(expected, actual);
+            // 3) assert
+            Assert.True(actual);
 		}
 
 		#endregion
 
 		#region GetHashCode Method Test
 
-		[TestMethod()]
-		public void Get_Hash_Code_ForEvent_Test()
+        [Fact]
+		public void Get_Hash_Code_For_Event_Test()
 		{
-			AEvent target = CreateEvent();
-
+            // 1) arrange
+            AEvent target = CreateEvent();
 			int expected = target.ID.GetHashCode();
-			int actual;
 
-			actual = target.GetHashCode();
-			Assert.AreEqual(expected, actual);
+            // 2) act
+			int actual = target.GetHashCode();
+
+            // 3) assert
+            Assert.Equal(expected, actual);
 		}
 
 		#endregion
