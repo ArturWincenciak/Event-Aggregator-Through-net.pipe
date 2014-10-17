4b893ba00163882e0497d7075d723e6d467bf7f1
TeoVincent <artur.wincenciak@gmail.com>
2014-10-17 12:54:50 +0200

    I added new project for TeoVincent.EventAggregator.Service project unit tests.

---
+++ TeoVincent.EventAggregator.Service.UnitTests/EventAggregatorServiceTester.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TeoVincent.EventAggregator.Service.UnitTests
+{
+    public class EventAggregatorServiceTester
+    {
+        private EventAggregatorService eventAggregator;
+
+        public EventAggregatorServiceTester()
+        {
+            eventAggregator = new EventAggregatorService();
+        }
+
+        [Fact]
+        public void Subscribe_Plugin_Test()
+        {
+            
+        }
+    }
+}
