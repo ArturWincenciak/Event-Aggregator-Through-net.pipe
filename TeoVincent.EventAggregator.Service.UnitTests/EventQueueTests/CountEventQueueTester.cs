2f58fa043d7762679eba3d5bd616858b23e76352
TeoVincent <artur.wincenciak@gmail.com>
2014-10-18 18:53:40 +0200

    I added few tests of count of event in queue.

---
+++ TeoVincent.EventAggregator.Service.UnitTests/EventQueueTests/CountEventQueueTester.cs
@@ -0,0 +1,81 @@
+using System;
+using TeoVincent.EventAggregator.Common.Events;
+using TeoVincent.EventAggregator.Common.Events.Tts;
+using TeoVincent.EventAggregator.Service.UnitTests.Mocks;
+using Xunit;
+
+namespace TeoVincent.EventAggregator.Service.UnitTests.EventQueueTests
+{
+    public class CountEventQueueTester : BaseEventQueueTester
+    {
+        [Fact]
+        public void Add_Event_Assert_Cout_Test()
+        {
+            // 2) act
+            queue.Enqueue(pluginName, e);
+            int actual = queue.GetCount(pluginName);
+            int expect = 1;
+
+            // 3) assert
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void Add_Two_Event_Assert_Cout_Test()
+        {
+            // 2) act
+            queue.Enqueue(pluginName, e);
+            queue.Enqueue(pluginName, new Event_Mock());
+            
+            int actual = queue.GetCount(pluginName);
+            int expect = 2;
+
+            // 3) assert
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void Add_Event_Assert_Cout_For_Enother_PluginName_Test()
+        {
+            // 1) arrange
+            string anotherName = "plugin";
+            
+            // 2) act
+            queue.Enqueue(pluginName, e);
+            int actual = queue.GetCount(anotherName);
+            int expect = 0;
+
+            // 3) assert
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void Add_Tree_Remove_Two_Assert_Cout_For_Test()
+        {
+            // 2) act
+            queue.Enqueue(pluginName, e);
+            queue.Enqueue(pluginName, new Event_Mock());
+            queue.Enqueue(pluginName, new Event_Mock());
+            queue.Dequeue(pluginName);
+            queue.Dequeue(pluginName);
+            int actual = queue.GetCount(pluginName);
+            int expect = 1;
+
+            // 3) assert
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void Add_Two_The_Same_Assert_Cout_For_Test()
+        {
+            // 2) act
+            queue.Enqueue(pluginName, e);
+            queue.Enqueue(pluginName, e);
+            int actual = queue.GetCount(pluginName);
+            int expect = 1;
+
+            // 3) assert
+            Assert.Equal(expect, actual);
+        }
+    }
+}
